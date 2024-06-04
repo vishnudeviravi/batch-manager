c497c05c2c3d81922d68f8d2c888dc8695616b02
iknoul <rhmanshamil@gmail.com>
2024-06-04 11:26:17 +0530

    fgas

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const testController = require('../controllers/testController');
-
+// dfgsg
 // test route
 router.get('/', testController.test);
 

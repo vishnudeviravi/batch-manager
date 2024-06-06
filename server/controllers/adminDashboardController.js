b5fcaad72d9e893cee7c45434bf2f2e3383ff12f
siva6168 <60339869+siva6168@users.noreply.github.com>
2024-06-06 13:24:13 +0530

    second commit

--- server/controllers/adminDashboardController.js
+++ server/controllers/adminDashboardController.js
@@ -11,3 +11,13 @@ router.get("admin/get-coordinator", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+
+//get all trainer
+router.get("admin/get-trainer", async (req, res) => {
+  try {
+    const trainer = await Trainer.find();
+    return res.status(200).json(trainer);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});

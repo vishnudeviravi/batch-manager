c18c3b4668f6fea9ec998ab7bd32eea8f4e87053
siva6168 <60339869+siva6168@users.noreply.github.com>
2024-06-06 13:18:34 +0530

    api commit

--- server/controllers/adminDashboardController.js
+++ server/controllers/adminDashboardController.js
@@ -1,1 +1,13 @@
-import express from 'express'
+import express from "express";
+
+const router = express.Router();
+
+//get all co-ordinators
+router.get("admin/get-coordinator", async (req, res) => {
+  try {
+    const coordinator = await Coordinator.find();
+    return res.status(200).json(coordinator);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});

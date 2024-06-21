d06b7d33c8b99cc6b536dff2165466b6faa474b1
SHAMIL RAHMAN <rhmanshamil@gmail.com>
2024-06-21 11:33:44 +0530

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,1 @@
-const express = require('express');
-const router = express.Router();
 
-const testController = require('../controllers/testController');
-// dfgsg
-// test route
-router.get('/', testController.test);
-
-module.exports = router;
